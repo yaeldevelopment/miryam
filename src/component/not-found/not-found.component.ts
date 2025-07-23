732500d4c40e8b427f5faad2b33efc5464658dee
yaelajami2 <y0556722091@gmail.com>
2025-07-23 09:51:08 +0300

    redirec+scss

--- src/component/not-found/not-found.component.ts
+++ src/component/not-found/not-found.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss'
 })
