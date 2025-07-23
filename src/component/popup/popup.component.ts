d996a6bb1b09f3fb8f9fe1dbe884d6a672c977e7
yaelajami2 <y0556722091@gmail.com>
2025-07-23 18:25:27 +0300

    setTimeout

--- src/component/popup/popup.component.ts
+++ src/component/popup/popup.component.ts
@@ -12,13 +12,16 @@ export class PopupComponent {
   showPopup = false;
 constructor(private router: Router) {}
   ngOnInit() {
-    const lastShown = localStorage.getItem('lastPopup');
+    setTimeout(() => {
+          const lastShown = localStorage.getItem('lastPopup');
     const now = new Date().getTime();
 
     if (!lastShown || now - parseInt(lastShown, 10) > 24 * 60 * 60 * 1000) {
       this.showPopup = true;
       localStorage.setItem('lastPopup', now.toString());
     }
+    }, 5000);
+
   }
 
   goToContact() {
